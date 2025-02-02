fd1a4d36984dc16d3e6507db0f67ca37306935d8
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-02-02 10:21:52 +0530

    adding expressjs file

--- expresss.js
+++ expresss.js
@@ -6,7 +6,19 @@ app.listen(port, () => { // listen makes the web server listen
     console.log(`App is listening on port ${port}`);
 });
 /// req ad res  request hi create krta h 
- 
+app.get("/", (req,res)=>{
+    res.send("you contacted root path");
+})
+app.get("/apple", (req,res)=>{
+    res.send("you contacted apple path");
+})
+app.get("/orange", (req,res)=>{
+    res.send("you contacted orange path");
+})
+app.get("*", (req,res)=>{
+    res.send("this page is not exist");
+})
+
 app.use((req,res)=>{
     console.log(req);
     let code = "<h1>MR CODER</h1>"
