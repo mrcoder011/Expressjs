fe832ea4a8fd72f4562ad1e4211c014b1ce2dbfa
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-02-04 18:46:12 +0530

    adding insta data

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const app = express();
+const port = 8080;
+
+// Set the view engine to EJS (if you plan to use EJS in the future)
 const path = require("path");
-const port = 8080  ;
-console.dir(app);
-
-// Set the view engine to EJS
-//app.set("view engine", "ejs");
-//app.set("view", path.join(___dirname,"/view"));
-
+// app.set("view engine", "ejs");
+ app.set("views", path.join(__dirname, "/views")); // yeh islie hota h taaki khin pr bhi jakr hm iska output run krwa ske
+app.set("view engine", "ejs");
 
 // Home route
-app.use( (req, res) => {
-    // res.render("home.ejs"); // Rendering the 'home.ejs' template
-    console.log(req);
-    res.send("this is a basic repsond ")
-
-});
+app.get("/", (req, res) => {
+   res.render("home.ejs");
+     });
+     app.get("/rolldice", (req, res) => {
+        let diceval =  Math.floor(Math.random()*6)+1;
+        res.render("rolldice", { diceval});
+          });
+          app.get("/ig/:username", (req, res) => {
+            const followers = ["adam","bob","steve","abc"]
+            let {username} = req.params;
+            res.render("instagram.ejs" ,{username , followers});
+              });
 
 // Start the server
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
- app
+ 
