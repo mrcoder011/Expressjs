eedcb3e0fb46e766cf9b1d1b62030b98d2c978f9
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-02-05 22:43:40 +0530

    adding insta.ejs

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 8080;
 
-// Set the view engine to EJS (if you plan to use EJS in the future)
-const path = require("path");
-// app.set("view engine", "ejs");
- app.set("views", path.join(__dirname, "/views")); // yeh islie hota h taaki khin pr bhi jakr hm iska output run krwa ske
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
-// Home route
 app.get("/", (req, res) => {
-   res.render("home.ejs");
-     });
-     app.get("/rolldice", (req, res) => {
-        let diceval =  Math.floor(Math.random()*6)+1;
-        res.render("rolldice", { diceval});
-          });
-          app.get("/ig/:username", (req, res) => {
-            const followers = ["adam","bob","steve","abc"]
-            let {username} = req.params;
-            res.render("instagram.ejs" ,{username , followers});
-              });
+    res.render("home.ejs");
+});
 
-// Start the server
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    console.log(data);
+    res.render("instagram.ejs", { data: instaData[username] });
+});
+
+app.get("/hello", (req, res) => {
+    res.send("hello");
+});
+
+app.get("/rolldice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", { diceVal });
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
- 
